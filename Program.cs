bbdd2e40d1bb783a477078b17004a0eba151e945
Amr Elsekilly <amr.e.dev@gmail.com>
2017-09-24 12:30:55 +0200

    created a product object

--- Program.cs
+++ Program.cs
@@ -9,4 +9,13 @@ namespace myApp
             Console.WriteLine("Hello World!");
         }
     }
+
+    // create a product class
+    class Product {
+        public int ID {get; set;}
+        public string Name {get; set;}
+        public double price {get; set;}
+    }
 }
+
+
